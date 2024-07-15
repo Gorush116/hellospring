2bdc45740a7bdfe3ed5397e78fa4237d6c90387b
Gorush116 <gorush34@gmail.com>
2024-07-15 11:07:24 +0900

    paymentService commit - 3

---
+++ src/main/java/tobyspring/hellospring/ExRateData.java
@@ -0,0 +1,11 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+// Json한테 다른 요소 들어있어도 무시
+@JsonIgnoreProperties(ignoreUnknown=true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+}
