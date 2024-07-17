0f8f165b077f1a7937068601eadf55a11c0396d2
Gorush116 <gorush34@gmail.com>
2024-07-17 11:01:17 +0900

    구성정보를 가져오는 다른 방법 commit

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,12 +1,16 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 // 구성정보를 포함하는 클래스임을 어노테이션을 통해 명시
 @Configuration
+// 컴포넌트 탐색 및 범위를 설정하는 어노테이션
+@ComponentScan
 public class ObjectFactory {
 
+/*  주석시 NoSuchBeanDefinitionException 발생(No qualifying bean of type 'tobyspring.hellospring.PaymentService' available)
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
@@ -17,5 +21,5 @@ public class ObjectFactory {
     public ExRateProvider exRateProvider() {
 //        return new WebApiExRateProvider();
         return new SimpleExRateProvider();
-    }
+    }*/
 }
