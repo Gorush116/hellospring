a1f0d6de62bc4f2aa9285fd32b0096cb5aef7631
Gorush116 <gorush34@gmail.com>
2024-07-17 10:52:12 +0900

    스프링 컨테이너와 의존관계 주입 commit

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,12 +1,21 @@
 package tobyspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 구성정보를 포함하는 클래스임을 어노테이션을 통해 명시
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    // Bean을 생성하는 메소드임을 명시
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
+//        return new WebApiExRateProvider();
+        return new SimpleExRateProvider();
     }
 }
