bfef18adbb32f04da337da3a2b0998acd279bc61
Gorush116 <gorush34@gmail.com>
2024-07-17 13:00:29 +0900

    싱글톤 레지스트리 commit

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -11,6 +11,22 @@ public class Client {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
+        // BeanFactory 통해 Bean 가져오기
+//        PaymentService paymentService2 = beanFactory.getBean(PaymentService.class);
+//        System.out.println("paymentService = " + paymentService);       // paymentService = tobyspring.hellospring.PaymentService@1e04fa0a
+//        System.out.println("paymentService2 = " + paymentService2);     // paymentService2 = tobyspring.hellospring.PaymentService@1e04fa0a
+//        System.out.println(paymentService == paymentService2);          // true
+
+        // ObjectFactory 통해 Bean 가져오기
+//        ObjectFactory objectFactory = beanFactory.getBean(ObjectFactory.class);
+//        PaymentService paymentService1 = objectFactory.paymentService();
+//        PaymentService paymentService2 = objectFactory.paymentService();
+//        System.out.println("paymentService1 = " + paymentService1);     // paymentService1 = tobyspring.hellospring.PaymentService@6a84a97d
+//        System.out.println("paymentService2 = " + paymentService2);     // paymentService2 = tobyspring.hellospring.PaymentService@6a84a97d
+//        System.out.println(paymentService == paymentService2);          // true
+
+//        OrderService orderService = beanFactory.getBean(OrderService.class);
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -5,21 +5,35 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 // 구성정보를 포함하는 클래스임을 어노테이션을 통해 명시
+// @Configuration을 통해 new 로 새로운 Object를 선언하여도 Proxy 를 통해 같은 Object를 사용하게끔 구현
 @Configuration
 // 컴포넌트 탐색 및 범위를 설정하는 어노테이션
-@ComponentScan
+// @ComponentScan
 public class ObjectFactory {
 
-/*  주석시 NoSuchBeanDefinitionException 발생(No qualifying bean of type 'tobyspring.hellospring.PaymentService' available)
+  // 주석시 NoSuchBeanDefinitionException 발생(No qualifying bean of type 'tobyspring.hellospring.PaymentService' available)
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
+
+//    @Bean
+//    public OrderService orderService() {
+//        return new OrderService(exRateProvider());
+//    }
 
     // Bean을 생성하는 메소드임을 명시
     @Bean
     public ExRateProvider exRateProvider() {
 //        return new WebApiExRateProvider();
         return new SimpleExRateProvider();
-    }*/
+    }
 }
+
+//class OrderService {
+//    ExRateProvider exRateProvider;
+//
+//    public OrderService(ExRateProvider exRateProvider) {
+//        this.exRateProvider = exRateProvider;
+//    }
+//}
