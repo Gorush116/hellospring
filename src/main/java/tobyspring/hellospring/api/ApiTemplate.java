009b6b6555a89ac4efa8133435bebea893b69138
Gorush116 <gorush34@gmail.com>
2024-07-25 15:47:33 +0900

    ApiTemplate 분리

---
+++ src/main/java/tobyspring/hellospring/api/ApiTemplate.java
@@ -0,0 +1,34 @@
+package tobyspring.hellospring.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ApiTemplate {
+    // 변경되지 않는 부분 시작
+    public static BigDecimal getExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
+        URI uri;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String response;
+        try {
+            response = apiExecutor.execute(uri);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            return exRateExtractor.extract(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    // 변경되지 않는 부분 끝
+}
