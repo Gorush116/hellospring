a2f6acf09100b1032c1095895448e67af7abf90e
Gorush116 <gorush34@gmail.com>
2024-07-26 15:52:41 +0900

    JDBC 데이터 액세스 기술 commit

--- src/main/java/tobyspring/hellospring/DataConfig.java
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -24,29 +25,9 @@ public class DataConfig {
         return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
     }
 
-    // entity manager factory 를 통해 entity 가 만들어짐
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(dataSource());        // data source set
-        emf.setPackagesToScan("tobyspring");    // scan
-        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter() {{
-            setDatabase(Database.H2);
-            setGenerateDdl(true);
-            setShowSql(true);
-        }});
-
-        return emf;
-    }
-
-    @Bean
-    public BeanPostProcessor PersistenceAnnotationBeanPostProcessor() {
-        return new PersistenceAnnotationBeanPostProcessor();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        return new JpaTransactionManager(emf);
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
     }
 
 }
