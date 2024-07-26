f790cd030ad348f436a534caf4dbaedb73792271
Gorush116 <gorush34@gmail.com>
2024-07-26 13:23:46 +0900

    DataAccessException과 예외 추상화 commit

--- src/main/java/tobyspring/hellospring/data/OrderRepository.java
+++ src/main/java/tobyspring/hellospring/data/OrderRepository.java
@@ -3,36 +3,16 @@ package tobyspring.hellospring.data;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.exception.ConstraintViolationException;
 import tobyspring.hellospring.order.Order;
 
 public class OrderRepository {
 
-    private final EntityManagerFactory emf;
-
-    public OrderRepository(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
+    @PersistenceContext
+    EntityManager entityManager;
 
     public void save(Order order) {
-        // entity manager
-        EntityManager em = emf.createEntityManager();
-
-        // transaction
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-
-        // em.persist 영속화 해달라 -> 작업 이후에도 그 변경사항이 남아있도록 처리
-        try {
-            em.persist(order); // 새로 만든 Object를 DB에 삽입
-            em.flush();
-
-            transaction.commit();
-        } catch (RuntimeException e) {
-            if (transaction.isActive()) transaction.rollback(); // transaction이 active라면
-            throw e;
-        } finally {
-            if (em.isOpen()) em.close();    // em이 열려 있다면
-        }
-
+        entityManager.persist(order); // 새로 만든 Object를 DB에 삽입
     }
 }
