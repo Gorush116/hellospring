f0ea079515216ec9525f16a8a8569815f1461ad5
Gorush116 <gorush34@gmail.com>
2024-07-26 11:06:52 +0900

    Order 리포지토리와 예외 commit

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -1,9 +1,8 @@
 package tobyspring.hellospring;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
@@ -11,22 +10,14 @@ import java.math.BigDecimal;
 public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
-        
-        // entity manager
-        EntityManager em = emf.createEntityManager();
-
-        // transaction
-        em.getTransaction().begin();
+        OrderRepository repository = beanFactory.getBean(OrderRepository.class);
 
         Order order = new Order("100", BigDecimal.TEN);
+        repository.save(order);
 
-        // em.persist 영속화 해달라 -> 작업 이후에도 그 변경사항이 남아있도록 처리
-        em.persist(order); // 새로 만든 Object를 DB에 삽입
+        Order order2 = new Order("100", BigDecimal.TWO);
+        repository.save(order2);
 
-        System.out.println(order);
-
-        em.getTransaction().commit();
-        em.close();
+        System.out.println("order = " + order);
     }
 }
