b0f3df2da12ae3ead885f9b6c602074bb1a41edd
Gorush116 <gorush34@gmail.com>
2024-07-29 10:43:15 +0900

    @Transactional과 AOP commit

--- src/main/java/tobyspring/hellospring/OrderConfig.java
+++ src/main/java/tobyspring/hellospring/OrderConfig.java
@@ -4,16 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.hellospring.data.JdbcOrderRepository;
 import tobyspring.hellospring.order.OrderRepository;
 import tobyspring.hellospring.order.OrderService;
 import tobyspring.hellospring.order.OrderServiceImpl;
-import tobyspring.hellospring.order.OrderServiceTransactionProxy;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement // 트랜잭션 관리 기능을 사용하게(web의 경우 Controller쪽)
 public class OrderConfig {
 
     @Bean
@@ -26,10 +27,6 @@ public class OrderConfig {
             PlatformTransactionManager transactionManager,
             OrderRepository orderRepository
     ) {
-        return
-                new OrderServiceTransactionProxy(
-                    new OrderServiceImpl(orderRepository),
-                    transactionManager
-                );
+        return new OrderServiceImpl(orderRepository);
     }
 }
