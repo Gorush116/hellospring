f22240664bea455d00a816ee4426a3deb8377066
Gorush116 <gorush34@gmail.com>
2024-07-30 13:10:48 +0900

    spring 연동관계 복습

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -3,13 +3,11 @@ package tobyspring.hellospring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ErApiExRateExtractor;
 import tobyspring.hellospring.api.SimpleApiExcutor;
 import tobyspring.hellospring.exrate.CachedExRateProvider;
 import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
 import tobyspring.hellospring.payment.ExRateProvider;
-import tobyspring.hellospring.exrate.WebApiExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
 
 import java.time.Clock;
